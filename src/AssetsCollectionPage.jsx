4c72058f0e24819ee3dc607b0bfbe5c6c9d29ff1
shawnmarkus <shawnmarkus4200@gmail.com>
2023-03-17 13:03:55 +0530

    added task title card

--- src/AssetsCollectionPage.jsx
+++ src/AssetsCollectionPage.jsx
@@ -1,12 +1,40 @@
 import React from "react";
 import data from "./Data.json";
 import AssetContainer from "./AssetContainer";
+import Card from "react-bootstrap/Card";
 
 const AssetsCollectionPage = ({ selected }) => {
   const task = data.tasks.find((itertask) => itertask.task_id === selected);
   return (
     <center>
-      <div className="gridEnclosingContainer">
+      <div className="enclosingBox">
+        <Card bg="Light" style={{ width: "100%" }} className="mb-4">
+          {/* <Card.Header>Header</Card.Header> */}
+          <Card.Body
+            style={{
+              backgroundColor: "#e9ecef",
+              textAlign: "left",
+            }}
+          >
+            <div
+              style={{
+                fontSize: "1.5rem",
+                fontWeight: 700,
+                padding: "1rem 0",
+              }}
+            >
+              {task.task_title}
+            </div>
+            <Card.Text
+              style={{
+                paddingBottom: "1rem",
+              }}
+            >
+              {task.task_description}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
         <div className="gridContainer">
           {task.assets.map((asset, indx) => {
             return (
